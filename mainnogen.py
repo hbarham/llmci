63d2bccb3c6c0d89e0f4957924f8c0519bb6c85d
hbarham <husam.barham@gmail.com>
2024-04-15 18:53:53 +0300

    better and smoother performance

--- mainnogen.py
+++ mainnogen.py
@@ -75,8 +75,8 @@ modely = YOLO("best.pt")
 # Initialize webdriver with default chrome withou user logging to chrome
 driver = webdriver.Chrome()
 # Set the window size (replace 'width' and 'height' with your desired values)
-driver.set_window_size(480, 800)
-# driver.set_window_size(1024, 1024)
+# driver.set_window_size(480, 800)
+driver.set_window_size(720, 1024)
 # Set the window position (replace 'x' with the horizontal position you want)
 driver.set_window_position(
     0, 0
@@ -91,7 +91,8 @@ def capture_window_screenshot():
 
     time.sleep(1)
     # Get the current window handle
-    # swindow_title = driver.current_window_handle
+    swindow_handle = driver.current_window_handle
+    driver.switch_to.window(swindow_handle)
     swindow_title = driver.title
     window = gw.getWindowsWithTitle(swindow_title)
     if len(window) == 0:
@@ -175,7 +176,7 @@ def screeninfo():
         # Merge the extracted text into a single string
         merged_text = " ".join(extracted_text_list)
 
-        print("extracted text:", merged_text)
+        # print("extracted text:", merged_text)
 
         # Get the center coordinates for each element
         x = int(x1 + (x2 - x1) / 2)
@@ -185,7 +186,7 @@ def screeninfo():
         detections_.append([x, y, class_name, merged_text])
 
     # print the list of complete elements detected wih their information
-    print(detections_)
+    # print(detections_)
 
     # Sort the elements detected asccending based on their y coordiante value
     detections_.sort(key=custom_sort)
@@ -194,14 +195,25 @@ def screeninfo():
         detection for detection in detections_ if detection[-1] != ""
     ]
 
-    # Get the title of the opened window
-    window_title = driver.title
-    # insert window title to the list
-    filtered_detections.insert(0, f"Window title is:{window_title}")
     # Get the current coordinate of the mouse
     m, p = pyautogui.position()
-    # Inser the current coordinate of mouse to the list
+    # Insert the current coordinate of mouse to the list
     filtered_detections.insert(0, f"Mouse Position: x={m}, y={p}")
+    # Get the URL of the current window
+    current_url = driver.current_url
+    current_url = truncate_string(current_url, 150)    
+    # insert window url
+    filtered_detections.insert(0, f"Window URL is:{current_url}")
+    # Get the title of the opened window
+    window_title = driver.title   
+    # insert window title to the list
+    filtered_detections.insert(0, f"Window title is:{window_title}")
+    # insert list of window handle
+    # window_handles = driver.window_handles
+    # filtered_detections.insert(0, f"List of selenium driver chrome tabs and their handles :{window_handles}")
+    # insert current window handle
+    # window_handle = driver.current_window_handle
+    # filtered_detections.insert(0, f"current window handle is (Check if new tab opened) :{window_handle}")
 
     # Index the sorted detections possible give LLM a id for each element instead of location..
     # indexed_sorted_detections = list(enumerate(filtered_detections))
@@ -248,7 +260,7 @@ def showscreen(show):
 def getelements(order):
 
     # Wait for the page to load completely
-    wait = WebDriverWait(driver, 10)  # Adjust the timeout (10 seconds)
+    wait = WebDriverWait(driver, 5)  # Adjust the timeout (10 seconds)
     wait.until(EC.presence_of_element_located((By.TAG_NAME, "body")))
 
     # capture window
@@ -327,11 +339,17 @@ def ochrome():
     wait = WebDriverWait(driver, 10)  # Adjust the timeout (10 seconds in this example)
     wait.until(EC.presence_of_element_located((By.TAG_NAME, "body")))
 
+# cut down long urls string
+def truncate_string(text, max_length):
+    if len(text) <= max_length:
+        return text
+    else:
+        return text[:max_length-3] + '...'
+
 
 # Define a custom sorting function to sort by y1 coordinate for elements in asscending order
 def custom_sort(detection):
     return detection[1]  # Sort based on the y1 coordinate
-
 
 # execute written code by LLM
 def execute(language, code):
@@ -339,14 +357,21 @@ def execute(language, code):
     # execute a code based on language
     result = coderexe.execute_code_blocks([(language, code)])
 
-    # get screen info after executing the code
-    screen_show = showscreen("What information shown in the webpage?")
-    screen_elements = getelements("order")
+    if "pyautogui" in code:
 
-    # return coderexe.execute_code_blocks([("sh", script)])
-    # messages = [{"content":code}]
+        # wait 1 second
+        driver.implicitly_wait(1)
 
-    return json.dumps(f"this is the result of code execution:{result}, and this is the updated page after code execution {screen_show}, and these are the updated elements of screen {screen_elements}")
+        # switch window in case new window opened
+        window_handle = driver.current_window_handle
+        driver.switch_to.window(window_handle)
+
+        # get screen info after executing the code
+        screen_show = showscreen("What information shown in the webpage?")
+        screen_elements = getelements("order")
+        return json.dumps(f"this is the result of code execution:{result}, and this is the updated page after code execution {screen_show}, and these are the updated elements of screen {screen_elements}")  
+    else:
+        return json.dumps(f"this is the result of code execution:{result}")
     
 
 # Enter python code to execute
@@ -373,7 +398,7 @@ functions = [
                 "type": "function",
                 "function": {
                 "name": "auto_execute_code",
-                "description": "Use this function to reach your goal by writing code using your coding and language skills. you can write python code (in a python coding block) or shell script (in a sh coding block) for the proxy system to execute it for you.",
+                "description": "Use this function to reach your goal by writing code using your coding and language skills. you can write python code (in a python coding block) or shell script (in a sh coding block to pip install) for the proxy system to execute it for you.",
                 "parameters": {
                     "type": "object",
                     "properties": {
@@ -399,7 +424,7 @@ functions = [
                     "properties": {
                         "url_or_search_term": {
                             "type": "string",
-                            "description": "Write only the <URL> or the search term",
+                            "description": "Write only the <URL> or the <search term>",
                         }
                     },
                     "required": ["url_or_search_term"],
@@ -409,13 +434,13 @@ functions = [
                 "type": "function",
                 "function": {
                 "name": "showscreen",
-                "description": "Use this function to request any information about what is shown in the screen, it returns content information about what is shown in the chrome window to help you collect information and possible functions/buttons in the page",
+                "description": "Use this function to request any information about what is shown in the screen, it returns content information about what is shown in the chrome window",
                 "parameters": {
                     "type": "object",
                     "properties": {
                         "show": {
                             "type": "string",
-                            "description": "ask detailed question what content and information are you looking for in the page",
+                            "description": "ask detailed question what content/elements and information are you looking for in the page",
                         }
                     },
                     "required": ["show"],
@@ -457,8 +482,8 @@ def chat_completion_request(messages, functions=functions, function_call='auto',
             messages=messages,
             tools=functions,
             tool_choice=function_call,
-            temperature=0.7,
-            frequency_penalty=0.3
+            temperature=0.6,
+            frequency_penalty=0.7
             )
     else:
         return client.chat.completions.create(
@@ -466,15 +491,14 @@ def chat_completion_request(messages, functions=functions, function_call='auto',
             messages=messages)
 
 
-def run_conversation(temp_m, is_log=False):
+def run_conversation(messages_temp, is_log=False):
     
-    messages_temp = temp_m
-    
+       
     while True:
         # Call the model to get a response
         response = chat_completion_request(messages_temp, functions=functions)
         response_message = response.choices[0].message
-
+        fresponse_message = None
         
         if is_log:
             print(response.choices)
@@ -492,10 +516,22 @@ def run_conversation(temp_m, is_log=False):
                        
             # Add the response to the temp messages
             
-            messages_temp = [item for item in temp_m if "ChatCompletionMessage" not in str(item)]
+            # t_temp = [item for item in messages_temp if "ChatCompletionMessage" not in str(item)]
             
-            messages_temp = [item for item in messages_temp if item.get('role') != 'tool']
+            # messages_temp = [item for item in t_temp if item.get('role') != 'tool']
                                                 
+            
+            if response_message.content:
+                
+                assistant_response = {"role": "assistant", "content": response_message.content}       
+                
+                messages_temp.append(assistant_response)
+                
+                color_print("*********** Agent temp response tool calling ***********", "yellow")
+                                                                
+                print(response_message.content)
+            
+            
             messages_temp.append(response_message)
                         
             messages_temp.append({
@@ -503,6 +539,21 @@ def run_conversation(temp_m, is_log=False):
                 "content": function_response,
                 "tool_call_id": response_message.tool_calls[0].id,
             })
+
+
+            color_print(f"*********** Tool call argument for tool ({function_name}) ***********", "yellow")
+                                                                
+            print(function_args)
+           
+
+            color_print(f"*********** Tool output for tool ({function_name}) ***********", "blue")
+                                                                
+            print(function_response)
+           
+
+            fresponse_message = response_message
+            
+            # print("These are the temp messages:\n", messages_temp)
 
             last_tool_id = response_message.tool_calls[0].id
 
@@ -513,19 +564,30 @@ def run_conversation(temp_m, is_log=False):
             
 
         else:
+            
             # No tool_calls, break the loop
             # Add the last function response to the conversation
+                        
+            t_temp = [item for item in messages_temp if "ChatCompletionMessage" not in str(item)]
+            
+            messages_temp = [item for item in t_temp if item.get('role') != 'tool']
+
+
+            if fresponse_message:            
+                messages_temp.append(fresponse_message)
+                            
+                messages_temp.append({
+                    "role": "tool",
+                    "content": function_response,
+                    "tool_call_id": fresponse_message.tool_calls[0].id,
+                })
             
                         
             # temp_m.pop(-1)
                         
             # del temp_m[-2:]
             
-            # print("the temp:",temp_m)
-            
-            
-
-            
+            # print("the temp:",temp_m)           
             
             # print("last messages: ", f_messages)
                                
@@ -539,6 +601,13 @@ def run_conversation(temp_m, is_log=False):
             # print("Response Message: ", response_message)
             
             # print("Response Message content: ", response_message.content)            
+            
+            
+            color_print(f"*********** LLMCI Response ***********", "green")
+                                                                
+            print(response_message.content)
+            
+            
             
             assistant_response = {"role": "assistant", "content": response_message.content}       
             
@@ -571,15 +640,14 @@ def filter_messages(c_messages):
 def run_chat(user_input="Hi"):
        
     system_message= """
-            You are a very helpful assistant for a blind person who cant see the computer screen, you will operate chrome instead of user to perform various types of activities              
-            You can use the provided functions call to help you code and execute code for you and user and feedback result, and to help you navigate URLs and to help you search google which will open a chrome window and search or enter the URL in chrome window. 
-            Use mouse keyboard code input to interact with chrome window elements 
-            chrome window list of object detection of elements on chrome window [h, v, text found in element based on OCR], window size 480x800, the results of elements info can be incomplete, interpretate elements based on content/location to understand their functionality and information in them so to operate web, Iterate again and use a different approach if you failed to achieve what you want.
-            You can ask for information about what is in the page by using the showscreen function and ask for what are you looking for in the screen.
-            Solve the tasks step by step to accomplish a project if you need to. If a plan is not provided, explain your plan first. Be clear which step uses code, and which step uses your language skill.
-            If you want to save a file in the system directory before executing for your project, put # filename: <filename> inside the code block as the first line. Don't include multiple code blocks in one response. Do not ask users to copy and paste the result. Instead, use 'print' function for the output when relevant or use file output. Check the execution result returned by the system.
-            If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code or code changes. If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, revisit your assumption, collect additional info you need using search/browse/showscreen/getelements functions, and think of a different approach to try.
-            When you find an answer, verify the answer carefully. Include verifiable evidence in your response if possible.
+            You are a very helpful assistant for a blind person who cannot see, you will operate chrome browser instead of user to perform various types of activities              
+            You can use the provided tools call to help you code and execute code for you and user and feedback result, and to help you navigate URLs and to help you search google which will open a chrome window and search or enter the URL
+            Write pyautogui to control mouse/keyboard input and interact with screen elements to browse internet and operate webapps.
+            Analyze the screen window window as a list of object detection of elements on screen window [h, v, text found in element based on OCR], window size 720x1024, the results of elements info may be incomplete, interpretate elements based on content/location to understand their functionality and information in them so to operate web, Iterate again and try to click on different location if you failed to achieve what you want such as if nothing changed after clicking, be sure to click on links and buttons not simple text so you be able to actually navigate the web.
+            Solve the tasks step by step to accomplish to best possible results, for example when you recieve google search results click on a link to find more information about your needs, if you see pop-ups such as accept cookies click as needed to continue with operating the webapp after, report back to user when you are satisfied with obtained results
+            You can also write and run code as needed, If you want to save a file in the system directory before executing for your project, put # filename: <filename> inside the code block as the first line. Don't include multiple code blocks in one response. Do not ask users to copy and paste the result. Instead, use 'print' function for the output when relevant or use file output. Check the execution result returned by the system, use 'print' function in code to check.
+            If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code or code changes. If desired result is not achieved, analyze the reason, revisit your assumption, collect additional info you need using different tools search/browse/showscreen/getelements, and think of a different approach to try.
+            When you find an answer, verify the answer. Include verifiable evidence in your response if possible.
 
             Reply TERMINATE when the task is completed.
         """
@@ -608,9 +676,25 @@ def run_chat(user_input="Hi"):
         
         # c_messages = filter_messages(c_messages)
         
-        print(c_messages)
+        # print(c_messages)
         
-         
+def color_print(text, color):
+    colors = {
+        'red': '\033[91m',
+        'green': '\033[92m',
+        'yellow': '\033[93m',
+        'blue': '\033[94m',
+        'purple': '\033[95m',
+        'cyan': '\033[96m',
+        'white': '\033[97m'
+    }
+    reset = '\033[0m'
+    if color in colors:
+        print(colors[color] + text + reset)
+    else:
+        print("Invalid color")
+
+        
 
 # An agent for executing code
 coderexe = autogen.UserProxyAgent(
